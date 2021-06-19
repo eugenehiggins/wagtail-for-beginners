17834077d44aa861dabd511e29de63caa185acb6
Gene Higgins <ehigginsiii@cnm.edu>
2021-06-19 14:23:27 -0600

    repeating struct blocks

--- home/models.py
+++ home/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from wagtail.core.fields import StreamField
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from streams import blocks
 
 
 class HomePage(Page):
@@ -29,10 +32,15 @@ class HomePage(Page):
         help_text='The banner background image',
         on_delete=models.SET_NULL,
     )
+    body = StreamField([
+        ("title", blocks.TitleBlock()),
+        ("cards", blocks.CardsBlock()),
+    ], null=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("lead_text"),
         PageChooserPanel("button"),
         FieldPanel("button_text"),
         ImageChooserPanel("banner_background_image"),
+        StreamFieldPanel("body"),
     ]
